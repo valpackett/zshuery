a13d5a0e9386d280098784a37e7d1f7ca68c2318
myfreeweb <me@myfreeweb.ru>
2011-03-16 21:27:55 +0300

    correction

--- zshuery.sh
+++ zshuery.sh
@@ -150,3 +150,19 @@ load_completion() { # thanks to Oh My Zsh and the internets
     zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
     zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
 }
+
+# Correction
+load_correction() {
+    setopt correct_all
+    alias man='nocorrect man'
+    alias mv='nocorrect mv'
+    alias mysql='nocorrect mysql'
+    alias mkdir='nocorrect mkdir'
+    alias erl='nocorrect erl'
+    alias curl='nocorrect curl'
+    alias rake='nocorrect rake'
+    alias make='nocorrect make'
+    alias cake='nocorrect cake'
+    alias lessc='nocorrect lessc'
+    SPROMPT="$fg[red]%R →$reset_color $fg[green]%r?$reset_color (Yes, No, Abort, Edit) "
+}
