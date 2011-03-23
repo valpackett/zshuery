88fd39da0b2cce8a173d03dddac39bb34c1d2170
myfreeweb <me@myfreeweb.ru>
2011-03-23 18:09:47 +0300

    github open alias

--- zshuery.sh
+++ zshuery.sh
@@ -130,6 +130,9 @@ load_aliases() {
     alias s_smtp='python -m smtpd -n -c DebuggingServer localhost:1025' # SMTP test server, outputs to console
     alias wget='wget --no-check-certificate'
     alias pinst='sudo python setup.py install && sudo rm -r build && sudo rm -r dist && sudo rm -r *egg-info' # install a Python package
+    if [ -e `which github` ]; then
+        alias gho="github open `git remote -v | sed -n '/github.com/p' | head -1 | sed 's/.git .*//;s/.*github.com[:\/]//'`"
+    fi
 }
 load_lol_aliases() {
     # Source: http://aur.archlinux.org/packages/lolbash/lolbash/lolbash.sh
