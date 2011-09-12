93e5f9f9c4df02ad233e03216229a2f6b0c1ee6d
myfreeweb <me@myfreeweb.ru>
2011-09-13 01:03:29 +0400

    moved all completions to files

--- zshuery.sh
+++ zshuery.sh
@@ -214,26 +214,6 @@ load_lol_aliases() {
 }
 
 # Completion
-_fab() { reply=(`fab --shortlist`) }
-_teamocil() { reply=(`ls ~/.teamocil | sed 's/.yml//'`) }
-_cap_does_task_list_need_generating() {
-  if [ ! -f .cap_tasks~ ]; then return 0;
-  else
-    accurate=$(stat -f%m .cap_tasks~)
-    changed=$(stat -f%m config/deploy.rb)
-    return $(expr $accurate '>=' $changed)
-  fi
-}
-_cap() {
-  if [ -f config/deploy.rb ]; then
-    if _cap_does_task_list_need_generating; then
-      echo "\nGenerating .cap_tasks~..." > /dev/stderr
-      cap show_tasks -q | cut -d " " -f 1 | sed -e '/^ *$/D' -e '1,2D'
-> .cap_tasks~
-    fi
-    compadd `cat .cap_tasks~`
-  fi
-}
 load_completion() { # thanks to Oh My Zsh and the internets
     autoload -U compinit
     fpath=($* $fpath)
@@ -242,9 +222,6 @@ load_completion() { # thanks to Oh My Zsh and the internets
     zmodload -i zsh/complist
     setopt complete_in_word
     unsetopt always_to_end
-    compctl -K _fab fab
-    compctl -K _teamocil teamocil
-    compctl -K _cap cap
     if [[ $HAS_BREW -eq 1 ]]; then
         compctl -K _gimme gimme
     fi
