228b3dcc784d2e75c7b8dae59c1a9f83c716ecc4
myfreeweb <floatboth@me.com>
2012-02-01 00:08:01 +0400

    removed stupid line

--- zshuery.sh
+++ zshuery.sh
@@ -6,7 +6,6 @@ if [[ $(uname) = 'Linux' ]]; then
 fi
 if [[ $(uname) = 'Darwin' ]]; then
     IS_MAC=1
-    export __CF_USER_TEXT_ENCODING=0x1F5:0x8000100:0x8000100
 fi
 if [[ -x `which brew` ]]; then
     HAS_BREW=1
