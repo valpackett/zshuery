185260d6824e44af09cb51eff47c84977f18bd37
myfreeweb <floatboth@me.com>
2014-08-15 01:33:40 +0400

    add iterm semantic history dir sequence

--- zshuery.sh
+++ zshuery.sh
@@ -78,10 +78,15 @@ elif [[ -d $RBENV_ROOT ]]; then
 else
     ruby_version() { echo '' }
 fi
-# Current directory in title
+# Current directory in title / iTerm semantic history (click on filename to open)
 if [[ $TERM_PROGRAM == "Apple_Terminal" ]]; then
     update_terminal_cwd() {
         printf '\e]7;%s\a' "file://$HOST$(pwd | sed -e 's/ /%20/g')"
+    }
+elif [[ $TERM_PROGRAM == "iTerm.app" ]]; then
+    # Only works without tmux or with integrated tmux
+    update_terminal_cwd() {
+        printf '\e]50;CurrentDir=%s\a' "$(pwd)"
     }
 else
     case $TERM in
