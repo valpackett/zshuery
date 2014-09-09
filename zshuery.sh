c5e73c050ecf0e289d002f270f14cee294bdff2e
myfreeweb <floatboth@me.com>
2014-09-09 18:58:15 +0400

    wtf what have i done lol

--- zshuery.sh
+++ zshuery.sh
@@ -246,7 +246,6 @@ load_aliases() {
     fi
     alias s_http='python -m SimpleHTTPServer' # serve current folder via HTTP
     alias s_smtp='python -m smtpd -n -c DebuggingServer localhost:1025' # SMTP test server, outputs to console
-    alias wget='wget --no-check-certificate'
     alias pinst='sudo python setup.py install && sudo rm -r build && sudo rm -r dist && sudo rm -r *egg-info' # install a Python package
     alias beep='echo -n "\a"'
     alias lst="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'"
