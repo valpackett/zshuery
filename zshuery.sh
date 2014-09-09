1860fc511be95a9ea310cc71d0cd7d2cb1c128be
myfreeweb <floatboth@me.com>
2014-09-09 18:58:43 +0400

    remove lol

--- zshuery.sh
+++ zshuery.sh
@@ -250,26 +250,6 @@ load_aliases() {
     alias beep='echo -n "\a"'
     alias lst="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'"
 }
-load_lol_aliases() {
-    # Source: http://aur.archlinux.org/packages/lolbash/lolbash/lolbash.sh
-    alias wtf='dmesg'
-    alias onoz='cat /var/log/errors.log'
-    alias rtfm='man'
-    alias visible='echo'
-    alias invisible='cat'
-    alias moar='more'
-    alias icanhas='mkdir'
-    alias donotwant='rm'
-    alias dowant='cp'
-    alias gtfo='mv'
-    alias hai='cd'
-    alias plz='pwd'
-    alias inur='locate'
-    alias nomz='ps aux | less'
-    alias nomnom='killall'
-    alias cya='reboot'
-    alias kthxbai='halt'
-}
 
 # Completion
 load_completion() {
